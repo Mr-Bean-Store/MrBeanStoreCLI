96c5b6786fc9b1ec7a82c4247c6210350231cb9e
Walter Nkosinathi Magagula <walter.magagula@bbd.co.za>
2024-03-13 22:25:53 +0200

    initial commit

---
+++ src/main/java/com/gp16/MrBeanStoreCLI/MrBeanStoreCliApplication.java
@@ -0,0 +1,13 @@
+package com.gp16.MrBeanStoreCLI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MrBeanStoreCliApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MrBeanStoreCliApplication.class, args);
+	}
+
+}
