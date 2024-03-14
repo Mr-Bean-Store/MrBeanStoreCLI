fe0386b374d989f753be2afe69399e9e62f1a245
Walter Nkosinathi Magagula <walter.magagula@bbd.co.za>
2024-03-14 09:01:39 +0200

    add weclome message and starter instructions

--- src/main/java/com/gp16/MrBeanStoreCLI/MrBeanStoreCliApplication.java
+++ src/main/java/com/gp16/MrBeanStoreCLI/MrBeanStoreCliApplication.java
@@ -1,13 +1,22 @@
 package com.gp16.MrBeanStoreCLI;
 
+import com.gp16.MrBeanStoreCLI.commands.login.LoginCommands;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.shell.command.annotation.EnableCommand;
 
 @SpringBootApplication
+@EnableCommand(LoginCommands.class)
 public class MrBeanStoreCliApplication {
 
 	public static void main(String[] args) {
+		System.out.println("================================================================================");
+		System.out.println("\t\t\t\t\t\t\tWelcome to Mr Bean Store");
+		System.out.println("================================================================================");
+
+		System.out.println("You can use the `help` command to see all the command you can use.");
+		System.out.println("Please use the `login` command to login with github and have access to our store.");
+
 		SpringApplication.run(MrBeanStoreCliApplication.class, args);
 	}
-
 }
